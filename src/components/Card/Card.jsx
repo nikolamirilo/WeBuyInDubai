e856a3fed894cbc3a233dfd73ae1b3e5b75d02e2
Nikola Mirilo <nikolamirilo@gmail.com>
2023-01-25 00:03:28 +0100

    added link from luxury watches card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,8 +1,19 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Card = (props) => {
+  const navigate = useNavigate();
   return (
-    <div className="card">
+    <div
+      className="card"
+      onClick={() => {
+        if (props.title[1] === "Luxury Watches.") {
+          navigate("/luxury-watches");
+        } else {
+          return null;
+        }
+      }}
+    >
       <img src={props.image} alt={props.title} />
       <div className="title">
         {props.type !== "luxury-watches" ? (
