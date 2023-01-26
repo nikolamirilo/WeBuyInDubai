de5e20ef2e07ddfdf8dda39279d954de6a30b0aa
nikolamirilo <nikolamirilo@gmail.com>
2023-01-26 14:35:23 +0100

    refactored hero component

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,9 +4,11 @@ const Hero = (props) => {
   return (
     <div className="hero" style={{ backgroundImage: `url(${props.image})` }}>
       <div className="heading">
-        {props.title.map((item, idx) => {
-          return <h1 key={idx}>{item}</h1>;
-        })}
+        <h1>
+          {props.title.map((item, idx) => {
+            return <div key={idx}>{item}</div>;
+          })}
+        </h1>
         <a href="tel:+971 12 345 6789">+971 12 345 6789</a>
       </div>
     </div>
